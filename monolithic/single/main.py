c8359d2da313741929c5ebf9f698fca0121a02a3
Yazan Kittaneh <yazan.kittane@progressoft.com>
2022-10-03 09:10:09 +0200

    - added battery simulator as a separate simulation to the mosaik co-simulation

--- monolithic/single/main.py
+++ monolithic/single/main.py
@@ -4,6 +4,12 @@ import random
 import mosaik
 from mosaik.util import connect_randomly, connect_many_to_one
 
+# todo: in the Meta data sent back by simulators when initiated, look into non-persistent attributes in mosaik 3.0,
+#  they are basically messages that are deleted from the buffer when consumed at a certain step. Consider them to
+#  send messages between simulators maybe?
+#  unlike attrs in Meta, which if there is no new value, the old value could be read/consumed again.
+#  Meaning it is a one time message. A transient value.
+
 logging.basicConfig()
 logger = logging.getLogger('demo')
 logger.setLevel(logging.INFO)
@@ -26,6 +32,10 @@ sim_config = {
         # 'connect': '0.0.0.0:8080',
         'connect': '127.0.0.1:5679',
     },
+    'BatterySimulator': {
+        # 'connect': '0.0.0.0:8080',
+        'connect': '127.0.0.1:5678',
+    },
 }
 
 START = '2014-01-01 00:00:00'
@@ -35,6 +45,7 @@ PROFILE_FILE = 'data/profiles.data-single.gz'
 # PROFILE_FILE = 'data/profiles.data-full.gz'
 GRID_NAME = 'demo_lv_grid'
 GRID_FILE = '%s.json' % GRID_NAME
+IS_BATTERY_SIMULATED = True
 
 
 def main():
@@ -43,8 +54,8 @@ def main():
     world = mosaik.World(sim_config)
     create_scenario(world)
     logger.info("Running world ...")
-    # world.run(until=END)  # As fast as possilbe
-    world.run(until=END, rt_factor=1 / 6000)  # Real_time_factor -- 1/60 means 1 simulation minute = 1 wall-clock second
+    world.run(until=END)  # As fast as possilbe
+    # world.run(until=END, rt_factor=1 / 6000)  # Real_time_factor -- 1/60 means 1 simulation minute = 1 wall-clock second
 
 
 def create_scenario(world):
@@ -53,7 +64,8 @@ def create_scenario(world):
     pypower = world.start('PyPower', step_size=15 * 60)
     pvsim = world.start('CSV', sim_start=START, datafile=PV_DATA)
     node_simulator = world.start('NodeSimulator', sim_start=START, eid='edgeNode', grid_name=GRID_NAME,
-                                 profile_file=PROFILE_FILE)
+                                 profile_file=PROFILE_FILE, is_battery_simulated=IS_BATTERY_SIMULATED)
+    battery_simulator = world.start('BatterySimulator', sim_start=START, eid='batteryNode', profile_resolution=15)
 
     # Instantiate models
     logger.info("Instantiating models ...")
@@ -72,10 +84,21 @@ def create_scenario(world):
     connect_randomly(world, pvs, [e for e in grid if 'node' in e.eid], 'P')
 
     # Connect edge node to grid
-    connect_node_to_grid(world, edge_nodes, buses)
+    connect_edge_node_to_grid(world, edge_nodes, buses)
     # Connect edge node to PV
-    connect_node_to_pv(world, edge_nodes, pvs)
+    connect_edge_node_to_pv(world, edge_nodes, pvs)
     # connect_randomly(world, pvs, edge_nodes, ('P', 'pv_power'))
+
+    # Battery
+    edge_grid_node_id = get_grid_node_id(world, edge_nodes)
+    battery_nodes = battery_simulator.Battery.create(1, grid_node_id=edge_grid_node_id)
+    # connect_battery_to_grid(world, battery_nodes[0], buses, edge_grid_node_id)
+    connect_battery_to_pv(world, battery_nodes[0], pvs[0])
+    connect_battery_to_edge_node(world, battery_nodes[0], edge_nodes[0])
+
+    ##############
+    world.connect(edge_nodes[0], battery_nodes[0], ('test', 'test'))
+    ##############
 
     # Database
     logger.info("Creating database ...")
@@ -83,6 +106,7 @@ def create_scenario(world):
     hdf5 = db.Database(filename='demo.hdf5')
     connect_many_to_one(world, pvs, hdf5, 'P')
     connect_many_to_one(world, edge_nodes, hdf5, 'P_out')
+    connect_many_to_one(world, battery_nodes, hdf5, 'current_load')
 
     nodes = [e for e in grid if e.type in ('RefBus, PQBus')]
     connect_many_to_one(world, nodes, hdf5, 'P', 'Q', 'Vl', 'Vm', 'Va')
@@ -143,6 +167,18 @@ def create_scenario(world):
         },
     })
 
+    connect_many_to_one(world, battery_nodes, vis_topo, 'current_load')
+    webvis.set_etypes({
+        'Battery': {
+            'cls': 'battery',
+            'attr': 'current_load',
+            'unit': 'P [W]',
+            'default': 0,
+            'min': -50000,
+            'max': 50000,
+        },
+    })
+
 
 def connect_buildings_to_grid(world, houses, buses):
     house_data = world.get_data(houses, 'node_id')
@@ -151,27 +187,51 @@ def connect_buildings_to_grid(world, houses, buses):
         world.connect(house, buses[node_id], ('P_out', 'P'))
 
 
-def connect_node_to_grid(world, nodes, buses):
+def connect_edge_node_to_grid(world, edge_nodes, buses):
     logger.info("***** inside connect_node_to_grid")
-    node_data = world.get_data(nodes, 'grid_node_id')
-    for node in nodes:
-        grid_node_id = node_data[node]['grid_node_id']
-        world.connect(node, buses[grid_node_id], ('P_out', 'P'))
+    edge_node_data = world.get_data(edge_nodes, 'grid_node_id')
+    for edge_node in edge_nodes:
+        grid_node_id = edge_node_data[edge_node]['grid_node_id']
+        world.connect(edge_node, buses[grid_node_id], ('P_out', 'P'))
         # todo (medium/high): the below connection seems wrong. I think P should not feed into grid_power because edgeNode.P_out feeds into gridNode.P, as seen in the above world.connect line. Need to figure out what P is.
-        world.connect(buses[grid_node_id], node, ('P', 'grid_power'), time_shifted=True, initial_data={'P': 0})
+        # world.connect(buses[grid_node_id], edge_node, ('P', 'grid_power'), time_shifted=True, initial_data={'P': 0})
 
 
-def connect_node_to_pv(world, edge_nodes, pvs):
+def connect_edge_node_to_pv(world, edge_nodes, pvs):
     logger.info("***** inside connect_node_to_pv")
     for node in edge_nodes:
         random.choice(pvs)
         world.connect(random.choice(pvs), node, ('P', 'pv_power'))
 
 
+def connect_battery_to_grid(world, battery, buses, edge_grid_node_id):
+    logger.info("***** inside connect_battery_to_grid")
+    grid_node = buses[edge_grid_node_id]
+    world.connect(grid_node, battery, ('P', 'grid_power'))
+
+
+def connect_battery_to_pv(world, battery, pv):
+    logger.info("***** inside connect_battery_to_pv")
+    world.connect(pv, battery, ('P', 'charge'))
+
+
+def connect_battery_to_edge_node(world, battery, edge_node):
+    # todo: does this connection make sense? Does the edge node need to know about the current load in the battery?
+    logger.info("***** inside connect_battery_to_edge_node")
+    world.connect(edge_node, battery, ('P_out', 'discharge'))
+    # world.connect(battery, edge_node, ('current_load', 'grid_power'))
+
+
 def get_buses(grid):
     buses = filter(lambda e: e.type == 'PQBus', grid)
     buses = {b.eid.split('-')[1]: b for b in buses}
     return buses
+
+
+def get_grid_node_id(world, edge_nodes):
+    logger.info("***** inside get_grid_node_id")
+    node_data = world.get_data(edge_nodes, 'grid_node_id')
+    return node_data[edge_nodes[0]]['grid_node_id']
 
 
 if __name__ == '__main__':
